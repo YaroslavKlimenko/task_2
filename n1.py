c1d0ea45b36c4d42be44c25e5bdcded37937ae0f
YaroslavKlimenko <yaroslav2005k@mail.ru>
2024-05-31 12:14:03 +0400

    commit3

--- n1.py
+++ n1.py
@@ -1,7 +1,8 @@
 def fib(n):
-    if n<3:
+    if n==1:
+        return 0
+    if n==2:
         return 1
-    return fib(n-1)+fib(n-2)
+    return fib(n-2)+fib(n-1)
+fib(int(input()))
 print("Hello, World!")
-n=int(input())
-print(fib(n))
